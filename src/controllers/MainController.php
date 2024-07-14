8174048254d94433fdb8eed49197a266f8707677
nyaga <c1223781@st.kanazawa-it.ac.jp>
2024-07-14 19:29:24 +0900

    値渡しに成功

--- src/controllers/MainController.php
+++ src/controllers/MainController.php
@@ -4,17 +4,15 @@ namespace controllers;
 
 use model\DataModel;
 
-include_once __DIR__ . "/../models/DataModel.php";
-include_once __DIR__ . "/../views/mainView.php";
+require_once __DIR__ . "/../models/DataModel.php";
 
 class MainController {
     public function display() {
-        echo "Controller before getting data.<br>"; // デバッグ出力
         $model = new DataModel();
-        global $data;
         $data = $model->getData();
-        echo "Controller after getting data: $data<br>"; // デバッグ出力
-        include_once(__DIR__ . "/../views/mainView.php");
+
+        // ビューにデータを渡す
+        require_once __DIR__ . "/../views/mainView.php";
     }
 }
 
--- src/views/mainView.php
+++ src/views/mainView.php
@@ -5,7 +5,7 @@
     <title>Sample Page</title>
 </head>
 <body>
-<p><?php global $data; echo isset($data) ? $data : 'No data provided'; ?></p>
+<p><?php echo isset($data) ? htmlspecialchars($data) : 'No data provided'; ?></p>
 <?php echo "<p>View loaded.</p>"; // デバッグ出力 ?>
 </body>
 </html>
